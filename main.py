1a33dff4edccee39f708c0a59b05a56c0bb406ee
Jacob Chamberlain <jacob.ryan.chamberlain@gmail.com>
2025-04-15 11:57:39 +1000

    End Kivy

--- main.py
+++ main.py
@@ -3,21 +3,27 @@ import kivy
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.anchorlayout import AnchorLayout
 
 
 class SetInputRow(BoxLayout):
-    def on_parent(self, instance, parent):
-        if parent:
-            parent.bind(height=self.update_height)
-            self.update_height(parent, parent.height)
+    # def on_parent(self, instance, parent):
+    #     if parent:
+    #         parent.bind(height=self.update_height)
+    #         self.update_height(parent, parent.height)
     
-    def update_height(self, instance, value):
-        self.height = value * 0.1
+    # def update_height(self, instance, value):
+    #     self.height = value * 0.1
+    pass
 
 
-class ExerciseSets(BoxLayout):
-    def add_row(self):
-        self.add_widget(SetInputRow(), index=1)
+class ExerciseSets(AnchorLayout):
+    def __init__(self):
+        super().__init__()
+        self.add_set()
+    
+    def add_set(self):
+        self.ids.set_container.add_widget(SetInputRow(), index=1)
 
 
 class WorkoutTrackerApp(App):
